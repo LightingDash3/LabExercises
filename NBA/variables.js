097aad8b80fc50f362b3ff2448083a10ed76600c
LightingDash3 <jasonreid@yahoo.com>
2023-02-17 13:14:37 +1000

    module 1

---
+++ NBA/variables.js
@@ -0,0 +1,82 @@
+
+// let variable =1;
+// let variable2;
+
+// if (true) {
+//     variable2 = 2;
+// }
+
+// function example(){
+//     let example = 33;
+//     const exampleConst = "example constant";
+
+//     console.log(exampleConst)
+//     console.log(exampleVar)
+
+// }
+
+
+// cosole.log(variable1)
+// console.log(variable2)
+
+// example()
+
+
+// const helloText = "say hello world"
+
+
+
+
+
+
+// const helloText = "say Hi";
+// helloText = "say Hello instead"; //error: Assignment to constant variable
+
+
+// const helloText = "say Hi";
+// const helloText = "say Hello instead";// error: Identifier 'greeting' has
+// already been declared
+
+
+
+
+
+// let mountains = ['Everest', 'Fuji', 'Nanga Parbat'];
+// console.log(mountains[0]); // 'Everest'
+// console.log(mountains[1]); // 'Fuji'
+// console.log(mountains[2]); // 'Nanga Parbat'
+
+
+// let seas = ['Black Sea', 'Caribbean Sea', 'North Sea', 'Baltic Sea'];
+// console.log(seas);
+// seas.push('Red Sea');
+// console.log(seas);
+
+// let pets = ['cats', 'dogs', 'fish'];
+// pets.unshift('lion')
+// console.log(pets)
+
+
+// let index = seas.indexOf('North Sea'); to find a index of 
+
+
+// console.log(Array.isArray(seas)); // true
+
+
+
+
+// const user = {
+//     "first_name" : "Sammy",
+//     "last_name" : "Shark",
+//     "age" : 25,
+//     "followers" : 987
+//     }
+//     console.log(user["first_name"]);
+//     console.log(user["age"])
+function sum(a, b){
+    return a+b;
+
+}
+
+if sum(2,4)
+!=6
