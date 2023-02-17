b5344fee9a5ee0cdb28934c654ea97c557ab10fc
LightingDash3 <jasonreid@yahoo.com>
2023-02-17 13:14:47 +1000

    module 1

---
+++ NBA/helloworld.js
@@ -0,0 +1,31 @@
+console.log('hello world')
+
+function sayHello() {
+    console.log('hello again')
+}
+
+sayHello()
+
+function sum(a,b) {
+    let result = a*b;
+    return result;
+}
+
+
+console.log(sum(88, 44))
+console.log(sum(123, 456))
+
+sayHello()
+
+
+function sum(a,b,c){
+
+
+let result = a+b*c;
+return result;
+}
+
+console.log (sum(365,52,12))
+console.log (sum(12,645,746))
+
+sayHello()
